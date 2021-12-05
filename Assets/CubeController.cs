d9790b86a8fadd6095732e0a3ec549ee2b00e876
ymsg3 <95019206+ymsg3@users.noreply.github.com>
2021-12-05 19:01:08 +0900

    課題後
    提出できる状態だと思ったので、プッシュ前にコミット。

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -28,4 +28,26 @@ public class CubeController : MonoBehaviour
             Destroy (gameObject);
         }
     }
+
+    // 課題用：
+    // このキューブが他のオブジェクトと衝突した時に呼び出される
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+
+        // キューブ落下地点付近でなければ何もしない
+        if (transform.position.x <= -2.9)
+        {
+            return;
+        }
+
+        // 衝突相手のタグの名前を取得
+        string tagName = collision.gameObject.tag;
+
+        // 衝突相手がキューブか地面なら、効果音を再生
+        if (tagName =="CubeTag"|| tagName =="GroundTag")
+        {
+            GetComponent<AudioSource> ().Play ();
+        }
+
+    }
 }
